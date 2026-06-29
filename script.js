44b74fcbe752b89bf424ca64ac34d5838dcfde86
dreadlordgamingyt-pixel <dreadlordgamingyt@gmail.com>
2026-06-29 23:07:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,24 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
         }
     });
 });
+const targetDate = new Date("July 12, 2026 20:00:00").getTime();
+
+setInterval(() => {
+
+    const now = new Date().getTime();
+
+    const distance = targetDate - now;
+
+    document.getElementById("days").innerHTML =
+    Math.floor(distance/(1000*60*60*24));
+
+    document.getElementById("hours").innerHTML =
+    Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+
+    document.getElementById("minutes").innerHTML =
+    Math.floor((distance%(1000*60*60))/(1000*60));
+
+    document.getElementById("seconds").innerHTML =
+    Math.floor((distance%(1000*60))/1000);
+
+},1000);
